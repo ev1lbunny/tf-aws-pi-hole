163d1cb0694f8fa9ec87a8a87d5541b5c45218f5
Karl Malkin <kmalkin@williamhill.co.uk>
2020-09-29 22:35:35 +0100

    KM-0001: added sgs , ip var and attached sgs to ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "pi_hole_ec2" {
   ami           = "ami-09a1e275e350acf38"
   instance_type = "t2.micro"
-
+  vpc_security_group_ids = [aws_security_group.pi_hole_security_group.id]
   depends_on = [module.iam_user]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,3 +11,7 @@ variable "pi_hole_management_role" {
 variable "aws_credentials_profile" {
   description = "High level rofile name that the terraform uses for aws access to create new role/policy/user for pihole deployment"
 }
+
+variable "ingress_access_ip_address" {
+  description = "Ip address that you will access the pihole from. All other access will be locked."
+}
