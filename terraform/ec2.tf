3f42d25cf1e38a002cfaa683fb84723b8d8a6594
Karl Malkin <kmalkin@williamhill.co.uk>
2020-09-29 22:09:32 +0100

    KM-0001: updated the ami id for the ubuntu ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "pi_hole_ec2" {
-  ami           = "ami-0c09927662c939f41"
+  ami           = "ami-09a1e275e350acf38"
   instance_type = "t2.micro"
 
   depends_on = [module.iam_user]
