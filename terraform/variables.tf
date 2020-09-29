01324735d17daebc4c71fefb9ab854e458641a57
Karl Malkin <kmalkin@williamhill.co.uk>
2020-09-29 22:11:10 +0100

    KM-0001: added precommit yaml

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,11 +8,6 @@ variable "pi_hole_management_role" {
   default     = "pi-hole-management-role"
 }
 
-variable "pi_hole_management_session_name" {
-  description = "Session name passed to the assume role when running setup"
-  default     = "Pi-hole Management Role Session"
-}
-
 variable "aws_credentials_profile" {
   description = "High level rofile name that the terraform uses for aws access to create new role/policy/user for pihole deployment"
 }
