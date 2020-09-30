2bb7528a5b6ed4f567677ac4b021ae175db98a1f
Karl Malkin <kmalkin@williamhill.co.uk>
2020-09-30 14:17:01 +0100

    KM-0001: bash var classhing with tf template var

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,20 +8,11 @@ resource "aws_key_pair" "generated_pi_hole_server_keypair" {
   public_key = tls_private_key.pi_hole_key_pair_config.public_key_openssh
 }
 
-data "template_file" "pi_hole_user_data" {
-  template = "${file("${path.module}/templates/pi-hole-init.tpl")}"
-  vars = {
-    public_ip          = var.ingress_access_ip_address
-    web_admin_password = var.pi_hole_web_admin_password
-  }
-}
-
 resource "aws_instance" "pi_hole_ec2" {
   ami                    = "ami-05c424d59413a2876"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.pi_hole_security_group.id]
   key_name               = var.key_pair_name
-  depends_on             = [module.iam_user]
   root_block_device {
     volume_size = 20
   }
@@ -34,4 +25,11 @@ resource "aws_instance" "pi_hole_ec2" {
 resource "aws_eip" "pi_hole_elastic_ip" {
   instance = aws_instance.pi_hole_ec2.id
   vpc      = true
+}
+
+data "template_file" "pi_hole_user_data" {
+  template = "${file("${path.module}/templates/pi-hole-init.tpl")}"
+  vars = {
+    web_admin_password = var.pi_hole_web_admin_password
+  }
 }
