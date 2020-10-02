392aa61f0f9f90be962c859a3036b79a9ec0cf41
Karl Malkin <kmalkin@williamhill.co.uk>
2020-10-02 14:16:48 +0100

    FR-0001: making modules optional

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,19 @@
 output "open_vpn_public_ip" {
   description = "OpenVPN Public IP created"
-  value       = module.open_vpn.public_ip
+  value       = module.open_vpn[0].public_ip
 }
 
 output "pi_hole_public_ip" {
   description = "Pi Hole Public IP created"
-  value       = module.pi_hole.public_ip
+  value       = module.pi_hole[0].public_ip
 }
 
 output "open_vpn_r53_entries" {
   description = "OpenVPN DNS Entries"
-  value       = module.open_vpn.r53_entry
+  value       = module.open_vpn[0].r53_entry
 }
 
 output "pi_hole_r53_entries" {
   description = "Pi Hole DNS Entries"
-  value       = module.open_vpn.r53_entry
+  value       = module.open_vpn[0].r53_entry
 }
